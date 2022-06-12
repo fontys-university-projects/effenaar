7129cd5998f5eb48a7d6591c4167f1d9e284e2e3
Dimitar Prisadnikov <i454903@fhict.nl>
2022-06-12 21:18:39 +0200

    added chat controller

---
+++ backend/controllers/chat.controller.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+
+    socket.emit('connections', Object.keys(io.sockets.connected).length);
+
+    socket.on('disconnect', () => {
+        console.log("A user disconnected");
+    });
+
+    socket.on('chat-message', (data) => {
+        socket.broadcast.emit('chat-message', (data));
+    });
+
+    socket.on('joined', (data) => {
+        socket.broadcast.emit('joined', (data));
+    });
+
+    socket.on('leave', (data) => {
+        socket.broadcast.emit('leave', (data));
+    });
+
+});
