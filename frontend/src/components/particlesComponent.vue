d202b84de93502ecb7f544be4fdd7c930e991783
plamski211 <plamski211@gmail.com>
2022-06-20 15:06:54 +0200

    stream page - particles + stream(switch button not done)

---
+++ frontend/src/components/particlesComponent.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="particles">
+    <div id="particles-js"></div>
+  </div>
+</template>
+
+<script>
+import 'particles.js'
+export default {
+  name: "particlesComponent",
+  mounted() {
+    this.initParticles()
+  },
+  methods: {
+    initParticles() {
+      window.particlesJS("particles-js", {
+                "particles": {
+                    "number": {
+                        "value": 80,
+                        "density": {
+                            "enable": true,
+                            "value_area": 700
+                        }
+                    },
+                    "color": {
+                        "value": "#ffffff"
+                    },
+                    "shape": {
+                        "type": "circle",
+                        "stroke": {
+                            "width": 0,
+                        },
+                    },
+                    "opacity": {
+                        "value": 0.5,
+                        "random": false,
+                        "anim": {
+                            "enable": true,
+                            "speed": 3,
+                            "opacity_min": 0.1,
+                            "sync": false
+                        }
+                    },
+                    "size": {
+                        "value": 5,
+                        "random": false,
+                        "anim": {
+                            "enable": false,
+                            "speed": 10,
+                            "size_min": 0.1,
+                            "sync": false
+                        }
+                    },
+                    "line_linked": {
+                        "enable": true,
+                        "distance": 150,
+                        "color": "#ffffff",
+                        "opacity": 0.4,
+                        "width": 1
+                    },
+                    "move": {
+                        "enable": true,
+                        "out_mode": "bounce",
+                        "speed": 6,
+                        "direction": "none",
+                        "random": false,
+                        "straight": false,
+                        "bounce": false,
+                        "attract": {
+                            "enable": false,
+                            "rotateX": 600,
+                            "rotateY": 1200
+                        }
+                    }
+                },
+                "interactivity": {
+                    "detect_on": "window",
+                    "events": {
+                        "onhover": {
+                                "enable": false,
+                                "mode": "repulse"
+                        },
+                        "onclick": {
+                            "enable": false,
+                            "mode": "push"
+                        },
+                        "resize": false
+                    },
+                    "modes": {
+                        "bounce": { distance: 200 },
+                        "grab": {
+                            "distance": 140,
+                            "line_linked": {
+                                "opacity": 1
+                            }
+                        },
+                        "bubble": {
+                            "distance": 400,
+                            "size": 40,
+                            "duration": 2,
+                            "opacity": 0.8,
+                        },
+                        "repulse": {
+                            "distance": 200,
+                            "duration": 0.4,
+                            "speed": 1
+                        },
+                        "push": {
+                            "particles_nb": 4
+                        },
+                        "remove": {
+                            "particles_nb": 2
+                        }
+                    }
+                },
+                "retina_detect": true
+            })
+    }
+  }
+};
+</script>
+
+<style scoped>
+#particles-js {
+  background: #0d47a1;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ frontend/src/views/StreamView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="app">
+       <iframe id="livestream" allow="autoplay" class="frame" src="https://www.youtube.com/embed/UBOj6rqRUME?autoplay=1&mute=1"
+            frameborder="0"
+            allowfullscreen>
+
+</iframe>
+    <particlesComponent/>
+  </div>
+ 
+</template>
+
+<script>
+import particlesComponent from '../components/particlesComponent.vue'
+
+
+export default {
+  name: "App",
+  components: {
+    particlesComponent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#livestream{
+   margin: auto;
+   height: 50vh;
+  width: 50%;
+  border: 3px solid green;
+  padding: 10px;
+}
+</style>
