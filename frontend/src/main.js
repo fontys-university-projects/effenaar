0aabe1143bea816862e6fa46f2f7e552f9b735dd
plamski211 <plamski211@gmail.com>
2022-06-20 15:09:37 +0200

    -

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,5 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import './index.css'
-import Particles from "particles.vue3";
 
-createApp(App).use(router, Particles).mount('#app')
+createApp(App).use(router).mount('#app')
